6eb6a5eb041e7b7a892db3f3d8ec5e8c60e9034e
gabriel-corto <digital.gabriel2006@gmail.com>
2025-01-06 21:15:52 +0100

    feat: finish app design

--- src/pages/app/Dashboard/popular-product.chart.tsx
+++ src/pages/app/Dashboard/popular-product.chart.tsx
@@ -41,6 +41,37 @@ export function PopularProductChart() {
               outerRadius={84}
               innerRadius={64}
               strokeWidth={8}
+
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product
+                      .substring(0, 12)
+                      .concat('...')}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+              labelLine={false}
               >
               {data.map((_, index) => {
                 return (
